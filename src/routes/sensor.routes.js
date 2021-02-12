e924e6cf750f361fe86e37aeec2c0e000232e540
Tarun yadav <tarunyadav9761@gmail.com>
2021-02-12 17:27:56 +0000

    added controller

---
+++ src/routes/sensor.routes.js
@@ -0,0 +1,15 @@
+import express from "express";
+
+const router = express.Router();
+
+router.route("").get(userCtrl.list).post(userCtrl.create);
+
+router
+    .route("/api/users/:userId")
+    .get(authCtrl.requireSignin, userCtrl.read)
+    .put(authCtrl.requireSignin, authCtrl.hasAuthorization, userCtrl.update)
+    .delete(authCtrl.requireSignin, authCtrl.hasAuthorization, userCtrl.remove);
+
+router.param("userId", userCtrl.userByID);
+
+export default router;
